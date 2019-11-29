91935d48294fe07db782c53497a089a7c4b634ef
valuoch <val.valentine227@gmail.com>
2019-11-29 10:16:45 +0300

    IMS

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('Iventory.html')
+
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
+
+
+
+if __name__ == '__main__':
+    app.run()
